af9681686a4b9655073482ee99c9c04c1db21c03
omicron <131537767+omicr0nn@users.noreply.github.com>
2023-12-07 23:10:58 +0300

    Update title.js

--- modules/title.js
+++ modules/title.js
@@ -1,6 +1,6 @@
 function console_title(x) {
     if (process.platform == 'win32') {
-        process.title = x + " / FastBomber v1.0 - github.com/omicr0n";
+        process.title = x + " / smsbomber - github.com/omicr0n";
     } else {
         process.stdout.write('\x1b]2;' + x + " / Sms Bomber v2.0 - github.com/omicr0nn" + '\x1b\x5c');
     }
